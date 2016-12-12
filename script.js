1274eea7c425e72602a40f670f448da38cbb6ce6
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-12-11 23:45:17 -0600

    working on the logic function to sort and randomly assign cards.

--- script.js
+++ script.js
@@ -13,19 +13,51 @@ $( document ).ready(function() {
       for(var i = 0; i < 12; i++){
         // var newCard = randomInt(0, 11);
         var currentCard = assignCard();
-        for(var card = 0; card < cardArray.length; card ++){
-          if(currentCard === cardArray[card]){
-            currentCard = assignCard();
-          } else {
-            cardArray.push(newCard);
-          }
+        console.log(currentCard);
+        // cardArray.push(currentCard);
+
+        if(i < 1){
+          pushCard(currentCard);
+        } else{
+          checkCardNumbers(currentCard);
         }
+
+        // for(var card = 0; card < 12; card ++){
+        //
+        //
+        //   if(currentCard === cardArray[card]){
+        //     currentCard = assignCard();
+        //   } else {
+        //     cardArray.push(currentCard);
+        //   }
+        // }
 
         // console.log(newCard);
       }
 
       console.log(cardArray);
 
+    }
+
+    function checkCardNumbers(current){
+      for(var i = 0; i < cardArray.length; i++){
+        console.log(cardArray[i] + " and the current is " + current);
+        console.log(cardArray.length);
+        if(current === cardArray[i]){
+          console.log("card numbers matched, re-do", current);
+          // current = assignCard();
+          return;
+        } else {
+          // pushCard();
+          console.log("card numbers did not match this is what current is", current);
+          pushCard(current);
+          return;
+        }
+      }
+    }
+
+    function pushCard(firstCard){
+      cardArray.push(firstCard);
     }
 
     function assignCard(){
