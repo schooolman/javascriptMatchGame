53233662636fab5d04f2622a2dc89a1e9c56a075
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-12-16 11:40:18 -0600

    cleaning up code

--- script.js
+++ script.js
@@ -37,31 +37,6 @@ $( document ).ready(function() {
       }
     }
 
-
-    //Functions
-
-
-    function checkCardNumbers(current){
-      for(var i = 0; i < cardArray.length; i++){
-        console.log(cardArray[i] + " and the current is " + current);
-        console.log(cardArray.length);
-        if(current === cardArray[i]){
-          console.log("card numbers matched, re-do", current);
-          // current = assignCard();
-          return;
-        } else {
-          // pushCard();
-          console.log("card numbers did not match this is what current is", current);
-          pushCard(current);
-          return;
-        }
-      }
-    }
-
-    function pushCard(firstCard){
-      cardArray.push(firstCard);
-    }
-
     function assignCard(max){
       var newCard = randomInt(0, max);
       return newCard;
@@ -71,18 +46,6 @@ $( document ).ready(function() {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    function checkForDuplicates() {
-      // console.log("checking");
-      for(var i = 0; i < cardArray.length; i++){
-        var selectedPosition = cardArray[i];
-        var testing = cardArray.indexOf(selectedPosition, "this is the selected position");
-        // console.log(testing);
-        // if(testing === -1){
-        //   console.log("found match");
-        // }
-      }
     }
 
   });
