8c9d8dc51b0caa080d455fd4b6b6daff0db5d8a0
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-12-16 12:10:00 -0600

    have card vaules appending to the DOM, using Angular.

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
 $( document ).ready(function() {
 
-    //This is the array of random card values built out.
-    var cardArray = [];
+    var app = angular.module('myApp', []);
 
-    //This variable is for the initial build of how many card paris for the game
-    var newCardArray = [];
+    app.controller('myApp', function($scope){
 
-    var numberOfCards = 12; //This variable will be adjusted by front-end user eventually
-    var numberOfPairs = numberOfCards/2;
+        // $scope.title = "Testing controller";
 
-    // var cardOne = 0;
-    // var cardTwo = 0;
-    // var cardThree = 0;
-    // var cardFour = 0;
-    // var cardFive = 0;
-    // var cardSix = 0;
+      //This is the array of random card values built out.
+      $scope.cardArray = [];
 
-    buildArray(numberOfPairs);
-    assignCards();
+      //This variable is for the initial build of how many card paris for the game
+      var newCardArray = [];
 
-    function assignCards(){
+      var numberOfCards = 12; //This variable will be adjusted by front-end user eventually
+      var numberOfPairs = numberOfCards/2;
 
-      for(var ii = 0; ii < numberOfCards; ii++){
-        var randomNumMax = newCardArray.length;
-        var tempCard = assignCard(randomNumMax);
-        var tempMove = newCardArray.splice(tempCard, 1);
-        cardArray.push(tempMove[0]);
+      // var cardOne = 0;
+      // var cardTwo = 0;
+      // var cardThree = 0;
+      // var cardFour = 0;
+      // var cardFive = 0;
+      // var cardSix = 0;
+
+      buildArray(numberOfPairs);
+      assignCards();
+
+
+      //functions
+
+      function buildArray(arrayLength){
+        for(var i = 0; i < arrayLength; i++){
+          newCardArray.push(i);
+          newCardArray.push(i);
+        }
       }
-      console.log(cardArray);
-    }
 
-    function buildArray(arrayLength){
-      for(var i = 0; i < arrayLength; i++){
-        newCardArray.push(i);
-        newCardArray.push(i);
+      function assignCards(){
+
+        for(var ii = 0; ii < numberOfCards; ii++){
+          var randomNumMax = newCardArray.length;
+          var tempCard = assignCard(randomNumMax);
+          var tempMove = newCardArray.splice(tempCard, 1);
+          $scope.cardArray.push(tempMove[0]);
+        }
+        console.log($scope.cardArray);
+        putCardsOnDOM();
       }
-    }
 
-    function assignCard(max){
-      var newCard = randomInt(0, max);
-      return newCard;
-    }
+      function putCardsOnDOM(){
+        for(var i = 0; i < $scope.cardArray.length; i++){
 
-    function randomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
+        }
+      }
 
+      function assignCard(max){
+        var newCard = randomInt(0, max);
+        return newCard;
+      }
+
+      function randomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+
+    });
   });
