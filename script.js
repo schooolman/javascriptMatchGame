e458940128cb207f91575e001f8d7f8a88e38027
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-12-16 11:39:27 -0600

    have a function now building an array based on amount of cards, and randomizing the cards within that array, I was apparently way overthinking it.

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 $( document ).ready(function() {
 
+    //This is the array of random card values built out.
     var cardArray = [];
 
+    //This variable is for the initial build of how many card paris for the game
     var newCardArray = [];
 
-    var numberOfCards = 12;
+    var numberOfCards = 12; //This variable will be adjusted by front-end user eventually
     var numberOfPairs = numberOfCards/2;
 
-    var cardOne = 0;
-    var cardTwo = 0;
-    var cardThree = 0;
-    var cardFour = 0;
-    var cardFive = 0;
-    var cardSix = 0;
+    // var cardOne = 0;
+    // var cardTwo = 0;
+    // var cardThree = 0;
+    // var cardFour = 0;
+    // var cardFive = 0;
+    // var cardSix = 0;
 
-    // randomCardAssign();
     buildArray(numberOfPairs);
     assignCards();
 
     function assignCards(){
-      var randomNumMax = newCardArray.length;
-      console.log(randomNumMax);
+
+      for(var ii = 0; ii < numberOfCards; ii++){
+        var randomNumMax = newCardArray.length;
+        var tempCard = assignCard(randomNumMax);
+        var tempMove = newCardArray.splice(tempCard, 1);
+        cardArray.push(tempMove[0]);
+      }
+      console.log(cardArray);
     }
 
     function buildArray(arrayLength){
@@ -28,101 +35,6 @@ $( document ).ready(function() {
         newCardArray.push(i);
         newCardArray.push(i);
       }
-      console.log(newCardArray);
-    }
-    // Using a switch is working as far as assigning and building a random
-    // card number array, but it is blowing the stack when the options get
-    // too small. Only works up to four cards.
-
-    // Need to remove the possiblility of a a loop that runs too long. Maybe
-    // selecting from an array and removing them from the choices when there are 2 of them?
-
-    function randomCardAssign(){
-
-      for(var i = 0; i < 12; i++){
-        // var newCard = randomInt(0, 11);
-        // var currentCard = assignCard();
-        var temp = assignCard();
-
-        switch (temp) {
-          case 0:
-            if(cardOne < 2){
-              cardArray.push(temp);
-              cardOne++;
-              console.log("hopefully Pushhed", cardOne);
-            } else {
-              console.log("no go");
-              i--;
-            }
-            break;
-
-          case 1:
-            if(cardTwo < 2){
-              cardArray.push(temp);
-              cardTwo++;
-              console.log("hopefully Pushhed", cardTwo);
-            } else {
-              console.log("no go");
-              i--;
-            }
-            break;
-
-          case 2:
-            if(cardThree < 2){
-              cardArray.push(temp);
-              cardThree++;
-              console.log("hopefully Pushhed", cardThree);
-            } else {
-              console.log("no go");
-              i--;
-            }
-            break;
-
-          case 3:
-            if(cardFour < 2){
-              cardArray.push(temp);
-              cardFour++;
-              console.log("hopefully Pushhed", cardFour);
-            } else {
-              console.log("no go");
-              i--;
-            }
-            break;
-
-          case 4:
-            if(cardFive < 2){
-              cardArray.push(temp);
-              // cardFive++;
-              console.log("hopefully Pushhed", cardFive);
-            } else {
-              console.log("no go");
-              i--;
-            }
-            break;
-
-          case 5:
-            if(cardSix < 2){
-              cardArray.push(temp);
-              // cardSix++;
-              console.log("hopefully Pushhed");
-            } else {
-              console.log("no go");
-              i--;
-            }
-            break;
-
-          default:
-            console.log("hitting default switch", temp);
-            break;
-
-        }
-
-        // cardArray.push(currentCard);
-
-      }
-      checkForDuplicates();
-      console.log(cardArray);
-
     }
 
 
@@ -150,8 +62,8 @@ $( document ).ready(function() {
       cardArray.push(firstCard);
     }
 
-    function assignCard(){
-      var newCard = randomInt(0, 5);
+    function assignCard(max){
+      var newCard = randomInt(0, max);
       return newCard;
     }
 
